bf05a7fb701cd79f6fcb847adcd3908f69176286
rajpipaliya048 <rajpipaliya2912@gmail.com>
2023-09-26 14:47:09 +0530

    task3 code has been changed

--- task3.py
+++ task3.py
@@ -1,40 +1,30 @@
-import re
+conditions = [
+    ("Username should have at least one '.'", lambda username: '.' in username),
+    ("Username should have at least one '_'", lambda username: '_' in username),
+    ("Username should be at least 8 characters long", lambda username: len(username) >= 8),
+    ("Password should have at least one of '@', '#', '$', '%'", lambda password: any(char in password for char in ['@', '#', '$', '%'])),
+    ("Password should have at least one uppercase letter", lambda password: any(char.isupper() for char in password)),
+    ("Password should have at least one lowercase letter", lambda password: any(char.islower() for char in password)),
+    ("Password should have at least one digit from 0-9", lambda password: any(char.isdigit() for char in password)),
+]
 
-# username check
-username = ""
-while True:
-    username = input("please enter username: ")
-    if len(username) < 9:
-        print("username is too short")
-        continue
-    if  username.find(".") == -1:
-        print("username should have atleast one '.'")
-        continue
-    if username.find("_") == -1:
-        print("username should have atleast one '_'")
-        continue
-    if (len(username) > 8 and username.find(".") != -1 and username.find("_") != -1):
+username = input("Enter a username: ")
+username_errors = [condition for condition, check in conditions[:3] if not check(username)]
+ 
+for error in username_errors:
+    print(error)
+    username = input("Enter a username: ")
+    if len(username_errors) == '0':
         break
     
-# password check
-password = ""
-pwdpattern = r"(?=.*[@#$%])(?=.*[A-Z])(?=.*[a-z])(?=.*\d)"
-while True:
-    password = input("please enter password: ")
-    if re.match(pwdpattern, password):
-        break
-    else:
-        if not re.search(r"[@#$%]", password):
-            print("Password should have at least one of '@', '#', '$', or '%'")
-            continue
-        if not re.search(r"[A-Z]", password):
-            print("Password should have at least one uppercase letter")
-            continue
-        if not re.search(r"[a-z]", password):
-            print("Password should have at least one lowercase letter")
-            continue
-        if not re.search(r"\d", password):
-            print("Password should have at least one digit")
-            continue
+password = input("Enter a password: ")
+password_errors = [condition for condition, check in conditions[3:] if not check(password)]
 
-print("\nUsername:", username,"\nPassword:", password )
+for error in password_errors:
+    print(error)
+    password = input("Enter a password: ")
+    if len(password_errors) == '0':
+        break
+print()
+print("Username is: %s" %username)
+print("Password is: %s" %password)
